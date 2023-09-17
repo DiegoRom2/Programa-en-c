21b54b224fd0ab90fed36cb9f4a7193df5648a0b
DiegoRom2 <143848582+DiegoRom2@users.noreply.github.com>
2023-09-17 11:58:27 -0600

    V2

--- CYP.cpp
+++ CYP.cpp
@@ -4,15 +4,15 @@
 #include <stdio.h>
 int main()
 {
-	int lado1;
-	int lado2;
+	int base;
+	int altura;
 	int area;
-	printf("Para calcular el area de un cuadrado los lados tienen que ser iguales\n");
-	printf("Lado1:");
-	scanf_s("%i", &lado1);
-	printf("Lado2:");
-	scanf_s("%i", &lado2);
-	area = lado1 * lado2;
-	printf("El area del cuadrado es de %i", area);
+	printf("Para calcular el area de un rectangulo los lados tienen que ser diferentes\n");
+	printf("Base:");
+	scanf_s("%i", &base);
+	printf("Altura:");
+	scanf_s("%i", &altura);
+	area = base * altura;
+	printf("El area del rectangulo es de %i", area);
 
 }
